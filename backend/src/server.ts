b1f0809d8d28081df87db756b07919348f8ccd5b
lucaxgomex <lg.projectdev@gmail.com>
2024-09-10 22:59:30 -0300

    Route for Disciplina

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,24 @@
 import fastity, { FastifyInstance } from "fastify";
+import { professorRoutes } from "./routes/professor.routes";
+import { disciplinaRoutes } from "./routes/disciplina.routes";
 
 const app: FastifyInstance = fastity(
     { 
         logger: false 
+    }
+);
+
+app.register(
+    professorRoutes,
+    {
+        prefix: '/users',
+    }
+);
+
+app.register(
+    disciplinaRoutes,
+    {
+        prefix: '/disciplina',
     }
 );
 
