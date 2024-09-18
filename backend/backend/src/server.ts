79a66a69f7d60a9ae2a4d631a6fefa94633ac897
lucaxgomex <lg.projectdev@gmail.com>
2024-09-18 19:08:26 -0300

    wip/development-backend: adding router for subjects

--- backend/backend/src/server.ts
+++ backend/backend/src/server.ts
@@ -1,23 +1,33 @@
 import fastify, { FastifyInstance } from "fastify";
 import cors from "@fastify/cors";   
-import { teacherRoutes } from "./presentation/routes/teacher.routes"
+import { teacherRoutes } from "./presentation/routes/teacher.routes";
 import { studentRoutes } from "./presentation/routes/student.routes";
+import { disciplinaRoutes } from "./presentation/routes/disciplina.routes";
 
-const app: FastifyInstance = fastify({ logger: true })
+
+const app: FastifyInstance = fastify({ logger: true });
 
 // Configurando CORS para permitir requisições de um frontend React
-app.register(cors, {origin: '*'})
+app.register(
+    cors, 
+    {
+        origin: '*'
+    }
+);
 
 app.register(teacherRoutes, {
     prefix: "/teachers"
-})
+});
 
 app.register(studentRoutes, {
     prefix: "/students"
-})
+});
 
-app.listen(
-    {
+app.register(disciplinaRoutes, {
+    prefix: '/disciplina'
+});
+
+app.listen({
         port: 3100
     },
     () => console.log("Server is running on port 3100")
