9f0f40a1dec52f0f847e5da32cd15fa167ba9c4f
lucaxgomex <lg.projectdev@gmail.com>
2024-09-19 01:03:26 -0300

    wip/development-reactjs-vite: added route to listing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage/Home";
-import ModalError from "./components/Modal/ModalError";
 import Error404 from "./pages/ErrorPage/Error404";
+import ListProfessoresComponent from "./pages/ErrorPage/List/ListProfessoresComponent/ListProfessoresComponent";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/modal" element={<ModalError/>}/>
+        <Route path="list/teachers" element={<ListProfessoresComponent/>}/>
         <Route path="*" element={<Error404/>}/>
       </Routes>
     </BrowserRouter>
