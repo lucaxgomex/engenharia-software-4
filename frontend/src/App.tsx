c67754c3cb6bdcf67b9f5967cb247ea62e45971c
lucaxgomex <lg.projectdev@gmail.com>
2024-09-22 18:38:20 -0300

    wip/development-reactjs-vite: defining route to list users page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage/Home";
 import Error404 from "./pages/ErrorPage/Error404";
-import ListProfessoresComponent from "./pages/ErrorPage/List/ListProfessoresComponent/ListProfessoresComponent";
+import ListProfessoresComponent from "./pages/ListPage/ListProfessoresComponent/ListProfessoresComponent";
 
 const App: React.FC = () => {
   return (
